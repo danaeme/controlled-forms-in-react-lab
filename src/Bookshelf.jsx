ced0f103510239a55af4b68ad31d1f9f6c956abd
David Anaeme <davidcanaeme@gmail.com>
2024-08-16 14:47:09 -0600

    Bookshelf functions

--- src/Bookshelf.jsx
+++ src/Bookshelf.jsx
@@ -13,7 +13,7 @@ const Bookshelf = () => {
         setNewBook({ 
             ...newBook, 
             [name]: value,
-        })
+        });
     };
 
     const handleSubmit = (event) => {
@@ -25,9 +25,32 @@ const Bookshelf = () => {
         <div className="bookshelfDiv">
         <div className="formDiv">
             <h3>Add a Book</h3>
-            {/* Form will go here */}
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="title">Title: </label>
+                <input
+                    id = "title"
+                    name = "title"
+                    value = {newBook.title}
+                    onChange = {handleInputChange}
+                />
+                <label htmlFor="author">Author: </label>
+                <input
+                    id="author"
+                    name="author"
+                    value={newBook.author}
+                    onChange={handleInputChange}
+                />
+                <button type="submit">Add Book</button>
+            </form>
+            </div>
+        <div className="bookCardsDiv">
+            {books.map((book, index) => (
+                <div key={index} className="bookCard">
+                    <h4>{book.title}</h4>
+                    <p>{book.author}</p>
+                </div>
+            ))}
         </div>
-        <div className="bookCardsDiv">{/* Book cards will display here */}</div>
         </div>
     )
 };
